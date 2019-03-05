3b42630f1471907c979c3f563fd3087ed827e85a
Amber Williams <Amber-Williams@users.noreply.github.com>
2019-03-05 21:17:37 +0100

    Feature: Text inputs are to uppercase on login

--- client/mmadness/src/login/login.js
+++ client/mmadness/src/login/login.js
@@ -9,22 +9,21 @@ export default class Login extends Component {
   
   userLogged(event){
     event.preventDefault();
-    const username = document.getElementById('userIs').value;
-    const roomCode = document.getElementById('roomCodeIs').value;
+    const username = document.getElementById('userIs').value.toUpperCase();
+    const roomCode = document.getElementById('roomCodeIs').value.toUpperCase();
     if(username.length > 0){
       this.props.emitUser(username, roomCode);
       this.props.history.push('/user')
     }
   }
 
-
   render() {
     return (
       <div className="loginContainer">
         <img className="logo" src={logo}/>
         <form action="">
-          <input className="whiteInput" id="roomCodeIs" placeholder="ROOMCODE" autoComplete="off" />
-          <input className="whiteInput" id="userIs" placeholder="NICKNAME" autoComplete="off" />
+          <input className="whiteInput toUpperCase" id="roomCodeIs" placeholder="ROOMCODE" autoComplete="off" />
+          <input className="whiteInput toUpperCase" id="userIs" placeholder="NICKNAME" autoComplete="off" />
           <button className="blackButton" onClick={this.userLogged.bind(this)}>Submit</button>
           <p> {this.props.error} </p>
         </form>
